74a746d89fbace63bca4d419bac508ad37a145b4
HunteRoi <me@tinaeldevresse.eu>
2022-01-23 15:53:41 +0100

    fix: trim filter

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -2,31 +2,31 @@ import React from 'react';
 import { FormControl, InputGroup } from 'react-bootstrap';
 
 type Props = {
-  value?: string,
-  setValue: (newFilter: string) => void
+	value?: string;
+	setValue: (newFilter: string) => void;
 };
 
 export const SearchField: React.FC<Props> = ({ value, setValue }) => {
-  const handleSearch = (event: any) => {
-    setValue(event.target.value);
-  }
+	const handleSearch = (event: any) => {
+		setValue(event.target.value.trim());
+	};
 
-  return (
-      <InputGroup className='mb-3 sticky-top'>
-        <InputGroup.Prepend>
-          <InputGroup.Text>
-            <span role='img' aria-labelledby='search emoji'>
-              🔍
-            </span>
-          </InputGroup.Text>
-        </InputGroup.Prepend>
-        <FormControl
-          type='text'
-          placeholder='Search'
-          onChange={handleSearch}
-          onPaste={handleSearch}
-          onDrop={handleSearch}
-        />
-      </InputGroup>
-  );
+	return (
+		<InputGroup className='mb-3 sticky-top'>
+			<InputGroup.Prepend>
+				<InputGroup.Text>
+					<span role='img' aria-labelledby='search emoji'>
+						🔍
+					</span>
+				</InputGroup.Text>
+			</InputGroup.Prepend>
+			<FormControl
+				type='text'
+				placeholder='Search'
+				onChange={handleSearch}
+				onPaste={handleSearch}
+				onDrop={handleSearch}
+			/>
+		</InputGroup>
+	);
 };
