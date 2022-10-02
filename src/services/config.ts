22068983d434e52ba367dd7be01051fdc4f28cb4
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2022-10-03 00:05:48 +0200

    upgrade to api v4

---
+++ src/services/config.ts
@@ -0,0 +1,5 @@
+export const fullAnimeAPI: string = "https://api.jikan.moe/v4";
+export const searchFullAnimeAPIUri: string = "/anime?q=";
+export const animeFullAnimeAPIUri: string = "/anime";
+
+export const streamingUrlAnimeAPI: string = "https://ajax.gogo-load.com/site/loadAjaxSearch?keyword=";
