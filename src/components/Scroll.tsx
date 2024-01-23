69ce13b4bc64f0da47fbe26bf56c870e0cc29b03
HunteRoi <etu33784@henallux.be>
2024-01-23 19:04:48 +0100

    fix: issue with element reference

--- src/components/Scroll.tsx
+++ src/components/Scroll.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import InfiniteScroll, { Props as InfiniteScrollProps } from 'react-infinite-scroll-component';
 
-export const Scroll: React.FC<InfiniteScrollProps> = ({ children, ...rest }) => <InfiniteScroll {...rest}>{children}</InfiniteScroll>;
+export const Scroll: React.ForwardRefExoticComponent<InfiniteScrollProps> = forwardRef<InfiniteScroll, InfiniteScrollProps>((props, ref) => (<InfiniteScroll {...props} ref={ref} />));
