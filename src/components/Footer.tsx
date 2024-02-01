03baa3a09abe9bd7868acd309d908ec437cfe44e
HunteRoi <me@tinaeldevresse.eu>
2024-02-01 20:43:04 +0100

    feat(Footer): add dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 export const Footer: React.FC = () => {
+  const year = useMemo(() => new Date().getFullYear(), []);
+  
   return (
-    <footer className='footer text-center mb-3'>&copy; HunteRoi 2020.</footer>
+    <footer className='footer text-center mb-3'>&copy; HunteRoi 2020-{year}.</footer>
   );
 }
